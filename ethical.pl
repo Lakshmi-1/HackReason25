424d53c0d7c6e09b4f3d12821a7c5fb6e50658de
Kashish Daulatani <daulatanik@gmail.com>
2025-01-16 23:47:08 -0600

    Ethical fix

--- ethical.pl
+++ ethical.pl
@@ -1,47 +1,27 @@
 % Facts
 car(left, 6).
 roadblock(right).
-pedestrians(left, 5). 
-animals(left, 6).
+pedestrians(Lane, Num_P) :- num_pedestrians(Lane, Num_P),!.
+pedestrians(Lane, 0).
+num_pedestrians(left, 5).
+animals(Lane, Num_A) :- num_animals(Lane, Num_A),!.
+animals(Lane,0).
+num_animals(left, 6).
 
 opposite_lane(left, right). 
 opposite_lane(right, left).
 
-pedestrians(Lane, 0).
-animals(Lane, 0).
-
 pedestrians_harmed(Lane, PH) :- pedestrians(Lane, H), PH is H.
 animals_harmed(Lane, AH) :- animals(Lane, H), AH is H.
-passengers_harmed(Lane, PaH) :- roadblock(Lane), car(_, H), PaH is H.
+passengers_harmed(Lane, PaH) :- roadblock(Lane),!, car(_, H), PaH is H.
 passengers_harmed(_,0).
 
 harm_stay(H) :- 
     car(Lane, _),
-    pedestrians_harmed(Lane, PH), not pedestrians_harmed(Lane, 0),
-    animals_harmed(Lane, AH), not animals_harmed(Lane, 0),
-    passengers_harmed(Lane, PaH), not passengers_harmed(Lane, 0),
+    pedestrians_harmed(Lane, PH),
+    animals_harmed(Lane, AH),
+    passengers_harmed(Lane, PaH),
     H is PH + AH + PaH.
-
-harm_stay(H) :- 
-    car(Lane, _),
-    pedestrians_harmed(Lane, 0),
-    animals_harmed(Lane, AH), not animals_harmed(Lane, 0),
-    passengers_harmed(Lane, PaH), not passengers_harmed(Lane, 0),
-    H is AH + PaH.
-
-harm_stay(H) :- 
-    car(Lane, _),
-    pedestrians_harmed(Lane, 0),
-    animals_harmed(Lane, 0),
-    passengers_harmed(Lane, PaH), not passengers_harmed(Lane, 0),
-    H is PaH.
-
-harm_stay(H) :- 
-    car(Lane, _),
-    pedestrians_harmed(Lane, 0),
-    animals_harmed(Lane, 0),
-    passengers_harmed(Lane, 0),
-    H is 0.
 
 harm_swerve(H) :- 
     car(Lane,_),
